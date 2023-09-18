82aff792647f5a67a274d0f84829716a8e80589c
Eric Couch <ericcouch@yahoo.com>
2023-09-18 16:36:06 -0500

    added linq queries

--- HPCTech2023FallCardGame/Deck.cs
+++ HPCTech2023FallCardGame/Deck.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,7 +29,42 @@ public class Deck
         return cardsDealt;
     }
 
+    public bool CheckForAce()
+    {
+        //return Cards.Any(card => card.rank == "A");
 
+        return Cards.Exists(card => card.rank == "A");
+        //return (from card in cards
+        //        where card.rank == "A"
+        //        select card).Count() > 0;
+    }
+
+    public bool? HasThreeOfAKind()
+    {
+        try
+        {
+            // method syntax
+            //bool? result = Cards.GroupBy(card => card.val).Any(group => group.Count() == 3);
+            // query syntax
+            bool? result = (from card
+                         in Cards
+                         group card by card.rank into g
+                         select new { rank = g.Key, count = g.Count() }).Any(c => c.count == 3);
+
+
+            return result;
+        } catch (Exception ex)
+        {
+            Console.WriteLine("An error occured");
+            return false;
+        }
+    }
+
+    public bool IsFlush()
+    {
+        // five cards of the same suit is a flush
+        return Cards.GroupBy(card => card.suit).Any(g => g.Count() == 5);
+    }
 
     public void ResetDeck()
     {
---
+++ HPCTech2023FallCardGameTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using HPCTech2023FallCardGame;
+
+namespace HPCTech2023FallCardGameTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestCardHandForAce_True()
+        {
+            // Arrange
+            Deck Hand = new Deck();
+            Hand.Cards = new List<Card>()
+            {
+                new Card("♦", "8", 8),
+                new Card("♣", "6", 6),
+                new Card("♦", "10", 10),
+                new Card("♦", "A", 14),
+                new Card("♠", "8", 8)
+            };
+
+            // Act
+            bool aceInHand = Hand.CheckForAce();
+
+            // Assert 
+            Assert.IsTrue(aceInHand);
+
+        }
+
+        [Test]
+        public void TestCardHandForAce_False()
+        {
+            // Arrange
+            Deck Hand = new Deck();
+            Hand.Cards = new List<Card>()
+            {
+                new Card("♦", "8", 8),
+                new Card("♣", "6", 6),
+                new Card("♦", "10", 10),
+                new Card("♦", "K", 13),
+                new Card("♠", "8", 8)
+            };
+
+            // Act
+            bool aceInHand = Hand.CheckForAce();
+
+            // Assert 
+            Assert.IsFalse(aceInHand);
+
+        }
+    }
+}
